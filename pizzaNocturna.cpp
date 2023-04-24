8869cd392a3b45a0d1b741a35037732b5bdd4c5d
FloresTM <seb.flores2002@gmail.com>
2023-04-24 17:12:24 -0600

    preparacion de archivos

---
+++ pizzaNocturna.cpp
@@ -0,0 +1,20 @@
+
+// Actividad 5.4 Problemas de sincronización
+// David Langarica Hernandez | A01708733
+// Sebastian Flores Lemus | A01709229
+#include <iostream>
+#include <iomanip>
+#include <pthread.h>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+// const int CELLS = 1000000000; //1e9
+const int THREADS = 8;
+
+int main(int argc, char* argv[]) {
+
+return 0;
+
+}
